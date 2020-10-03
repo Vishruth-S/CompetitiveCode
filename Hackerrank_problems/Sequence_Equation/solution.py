f019a59d22211f7519609ead13292c5b88dcc548
vivax3794 <vivax3794@users.noreply.github.com>
2020-10-03 17:40:32 +0200

    improved explination

--- Hackerrank_problems/Sequence_Equation/solution.py
+++ Hackerrank_problems/Sequence_Equation/solution.py
@@ -8,10 +8,18 @@ import sys
 
 # Complete the permutationEquation function below.
 def permutationEquation(p):
+    # list to store all the values of y
     results = []
+    # loop over all values in: 1 <= x <= n (were n is the lenght of p)
     for x in range(1, len(p) + 1):
         # get the index of the index of x.
+        # to take the example from the problem: p = [5, 2, 1, 3, 4], x=1
+        # p.index(1) returns 2, we add one to get 3.
+        # p.index(3) returns 4, we add one to get 5.
+        # and we just do this for all values of x
         results.append(p.index(p.index(x) + 1) + 1)
+
+    # return all the found y
     return results
 
 
