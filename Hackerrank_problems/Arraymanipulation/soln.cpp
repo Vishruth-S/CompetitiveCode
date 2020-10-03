8df7b08b45a8ab1d982c30895a75d060b83abc06
AnshMishra2001 <55917516+AnshMishra2001@users.noreply.github.com>
2020-10-03 21:53:42 +0530

    Update soln.cpp

--- Hackerrank_problems/Arraymanipulation/soln.cpp
+++ Hackerrank_problems/Arraymanipulation/soln.cpp
@@ -5,6 +5,13 @@
 #include <algorithm>
 using namespace std;
 
+/* LOGIC:
+see, you are adding sum to a[p] and adding negative sum at a[q+1].
+ which make sure that when you add element from a[p] to a[q] sum is added only once and 
+ it should be subtracted at a[q+1] as this sum span from p to q only. 
+ Rest array element are either 0 or some other input sum. max of addition will be output. 
+ refer to above code for p, q, and sum*/
+
 
 int main() {
     long int N,K,p,q,sum,i,j,max=0,x=0;
