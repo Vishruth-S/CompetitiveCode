147fca90922270fb69e1daacf02a5f4b2876f2ea
Karl Marx <51373628+karlmarx@users.noreply.github.com>
2020-10-03 18:40:50 -0400

    Update solution.py

--- Hackerrank_problems/Triangle_Quest/solution.py
+++ Hackerrank_problems/Triangle_Quest/solution.py
@@ -1,3 +1,4 @@
-# Used math to complete in 2 lines as required by problem.
+# Used the fact that 9ths as decimals have repeating digits (1/9=.11111, 2/9=.22222) with powers of 10 to get the correct length of each number.  
+# Completed in 2 lines as required by problem.
 for i in range(1,int(input())): 
     print((10**(i)//9)*i)
