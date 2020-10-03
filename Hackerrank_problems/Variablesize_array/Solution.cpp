a2182d7f30f97475e5fc6299c1354c50d4cb890f
shashankseth01 <56696758+shashankseth01@users.noreply.github.com>
2020-10-03 23:13:33 +0530

    Update Solution.cpp

--- Hackerrank_problems/Variablesize_array/Solution.cpp
+++ Hackerrank_problems/Variablesize_array/Solution.cpp
@@ -1,3 +1,6 @@
+//Author : @shashankseth01
+//Simple solution for variablesize array in c++ language
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -27,3 +30,10 @@ int main() {
     }
     return 0;
 }
+// used 'new' opeator for dynamic memory allocation ,if you check out the problem statement 
+// you will encounter that the array elements are not taken in a continuous manner form the input
+// first the size of the 1D array is given and then the array elemnts
+// So, coming back to solution I have defined an array pointer(that holds the number of rows)
+// At the time of execution the size of row is decided by 'k' ,for that 'n' we make an array of size 'k' at that position.
+// So, basically we are trying to make it a 2D array using dynamic memory alocation.   
+// and rest is just input and output.
