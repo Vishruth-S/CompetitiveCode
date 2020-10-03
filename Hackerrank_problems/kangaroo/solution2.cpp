a0da1b168e11d34669520390eb69280a45977b6d
Saujanya Pandey <saujanya.pandey2000@gmail.com>
2020-10-03 13:49:54 +0530

    Added logic of kangaroo solution.

---
+++ Hackerrank_problems/kangaroo/solution2.cpp
@@ -0,0 +1,15 @@
+// LOGIC:
+// First, we compare the first kangaroo's location & rate with the second one.
+//      if the first kangaroo's location & rate both are higher than the second or vice versa,
+//      there's no way both will reach same location at any point.
+// Second, if the location & rate of both the kangaroos are same, 
+//      then they will both reach at the same time.
+// Third, if both the kangaroos have same location but dofferent rates,
+//      then again no chance of reaching together.
+// ELSE, we calculate a variable 'y' which can make both their final locations same.
+// Then we check if the value of 'y' can satisfy the given location & rate value.
+//  If yes, then they reach together
+// Else they don't.
+
+// CODE:
+
