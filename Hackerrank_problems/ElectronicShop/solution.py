346726c65620a463af42fd43a05670fb27dc9a19
MeFaltaUnVerano <trolleros200@gmail.com>
2020-10-03 11:26:41 +0200

    Update solution.py

--- Hackerrank_problems/ElectronicShop/solution.py
+++ Hackerrank_problems/ElectronicShop/solution.py
@@ -4,11 +4,10 @@ import os
 import sys
 
 #
-# Complete the getMoneySpent function below.
-#
+#You will get the money spent between the keyboard and the usb and sum it if it is less than the budget it will return the price spent
 def getMoneySpent(keyboards, drives, b):
     spend = -1
- 
+ #Check each cost of each keyboard with that of each usb will sum them until some result is less than the budget we have
     for key in keyboards:
         for d in drives:
             if key + d <=b:
@@ -16,6 +15,7 @@ def getMoneySpent(keyboards, drives, b):
  
     return spend
 
+#Get the input from the user the first line will get the max money to spend, the model of keyboard and the model of usb
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     #
     # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
     #
-
+    #It will start the function to obtain the spent price
     moneySpent = getMoneySpent(keyboards, drives, b)
 
     fptr.write(str(moneySpent) + '\n')
