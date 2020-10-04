acfbbd61510a47318446b833ced9971aed9604bd
Sahil Jena <68604369+sahiljena@users.noreply.github.com>
2020-10-04 22:36:29 +0530

    Update SC_02.py
    Added comments explaining the solution

--- CodeChef_problems/SCAT2020/SC_02.py
+++ CodeChef_problems/SCAT2020/SC_02.py
@@ -1,21 +1,22 @@
-# cook your dish here
 #PROBLEM : https://www.codechef.com/SCAT2020/problems/SC_02
-a = int(input())
-for i in range(a):
-    n = int(input())
-    b = list(map(int,input().split()))
-    g = list(map(int,input().split()))
-    b.sort()
-    g.sort()
-    h1 =[]
-    h2= []
-    for i in range(n):
+#ORIGINAL SOLUTION : https://www.codechef.com/SCAT2020/status/SC_02,sahiljena
+
+a = int(input()) #Input for number of testcases
+for i in range(a): 
+    n = int(input()) #input for number of boys and girls in the classroom
+    b = list(map(int,input().split())) #input height of boys
+    g = list(map(int,input().split())) #input height of girls
+    b.sort()  #sorting height of boys
+    g.sort()  #sorting height of girls
+    h1 =[]  #Empty List with height starting with boys ,ie. B1,G1,B2,G2 to be updated later
+    h2= []  #List with height starting with girls ,ie. G1,B1,G2,B2 to be updated later
+    for i in range(n): #Inserting Heights of boys and girls into list h1 (Starting from Boys ie. B1,G1,B2,G2)
         h1.append(b[i])
         h1.append(g[i])
-    for i in range(n):
+    for i in range(n): #Inserting Heights of boys and girls into list h1 (Starting from girls ie. G1,B1,G2,B2)
         h2.append(g[i])
         h2.append(b[i])
-    if h1 == sorted(h1) or h2 ==  sorted(h2):
-        print("YES")
+    if h1 == sorted(h1) or h2 ==  sorted(h2):  #--Main Logic-- to check if arranged height order of boys and girls is sorted 
+        print("YES") #If any of the arranged height order is sorted then output "YES"
     else:
-        print("NO")
+        print("NO") # If None of the arrnaged height order is sorted then output "NO"
