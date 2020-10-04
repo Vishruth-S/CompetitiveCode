3dda362983ebb9c15623eba8958ebc24891321ff
harshlohia11 <harsh.lohia11@gmail.com>
2020-10-04 22:14:41 +0530

    Add leetcode monotonic array code

--- LeetCode_problems/Monotonic Array/solution_1.py
+++ LeetCode_problems/Monotonic Array/solution_1.py
@@ -1,5 +1,8 @@
 class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
+        #an array is monotonic if it is either increasing or decreasing, so we check if the array is equal to 
+        #its sorted version in either descending order or ascending order. If yes return True
+        #else return False.
         if A==sorted(A):
             return True
         if A==sorted(A,reverse=True):
