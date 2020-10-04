f4507e8e6129489750556f9ff899e82d7744e13c
Ajay-Kanojiya <61092620+Ajay-Kanojiya@users.noreply.github.com>
2020-10-04 20:00:22 +0530

    Create Solution1.py

---
+++ Hackerrank_problems/Array Manipulation/Solution1.py
@@ -0,0 +1,33 @@
+
+import os
+from itertools import accumulate
+
+def arrayManipulation(n, queries):
+   
+    
+    n = [0] * (n + 1)                           # allow run past end
+
+    for i in range(len(queries)):
+        n[queries[i][0] - 1] += queries[i][2]
+        n[queries[i][1]] -= queries[i][2]
+
+    return max(accumulate(n))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
