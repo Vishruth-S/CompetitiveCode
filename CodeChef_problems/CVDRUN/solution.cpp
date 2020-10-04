e45dc3096524688760e2c62bf998c40b0370045b
ganesh <soccerganesh@gmail.com>
2020-10-05 00:13:48 +0530

    logic added to covid run solution

--- CodeChef_problems/CVDRUN/solution.cpp
+++ CodeChef_problems/CVDRUN/solution.cpp
@@ -1,3 +1,12 @@
+/*
+    The logic is to run the program a minimum number of times, i.e. n times in order
+    to check if the city of spread reaches the desired city
+
+    Thus we run the loop and increment x by k over n iterations to check if it ever equals k
+
+    If it does, we set an infect tag and print "YES"
+*/
+
 #include<iostream>
 using namespace std;
 int main() {
