212d74b734f71bd6e413c2fc0ad5d35d3feff8e1
yashsn <35648647+yashsn@users.noreply.github.com>
2020-10-04 21:26:26 +0530

    Comments updated

--- LeetCode_problems/Count Square Submatrices with All Ones/solution.java
+++ LeetCode_problems/Count Square Submatrices with All Ones/solution.java
@@ -5,11 +5,14 @@ class Solution {
         n=matrix[0].length;
         int h=Math.min(m,n);
         int[][] ans=new int[m+1][n+1];
+        //using Dynamic Programming to solve this problem, thus need storage matrix
         for(i=1;i<=m;i++){
             for(j=1;j<=n;j++){
-                if(matrix[i-1][j-1]!=0){
+                if(matrix[i-1][j-1]!=0){ //if the cell contains 1
                     sm+=(ans[i][j]=Math.min(Math.min(ans[i-1][j],ans[i][j-1]),ans[i-1][j-1])+1);
-                    
+                    //in the ans matrix, put the minimum of its i-1 (previous row) and j (same column ) or i(same row)
+                    //and j-1(prev column) or minimum of that against min of i-1 (prev row) and j-1 (prev column) 
+                    //one since this is additional case of 1 has to be added
                 }
             }
         }
