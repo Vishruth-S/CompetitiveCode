53df3aab570fbda439a478739795f59c7b5b33a8
Arnav Sachdeva <34678814+arnav1598@users.noreply.github.com>
2020-10-05 17:37:43 +0530

    Update solution.cpp

--- CodeChef_problems/The Tom and Jerry Game!/solution.cpp
+++ CodeChef_problems/The Tom and Jerry Game!/solution.cpp
@@ -1,3 +1,10 @@
+/*
+    Dividing TS till an odd number is reached and counting the number of divisions (count).
+    In order for JS to win, the smallest such number will have to be divided the same number of times till even. (Resulting in TS being odd and JS being even, so JS wins - first such occurrence)
+    Smallest such number will therefore be 2^(count +1)
+    Winning numbers will be a list of multiples of this smallest number and less than TS.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
