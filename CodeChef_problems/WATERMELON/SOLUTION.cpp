b6209c6d0b77e21dfe7174f00e7425536bb85e41
dikshajoshi18 <dikshajoshi1800@gmail.com>
2020-10-05 12:15:40 +0530

    codechef

--- CodeChef_problems/WATERMELON/SOLUTION.cpp
+++ CodeChef_problems/WATERMELON/SOLUTION.cpp
@@ -12,15 +12,20 @@ int main() {
 	    int n,sum=0;
 	    cin>>n;
 	    int arr[n+2];
-	    
+//i have taken an array with increased size because sometimes tight arrays throws bound error
+//and in this case it was    
 
 	    for(int i=0;i<n;i++){
 	        cin>> arr[i];
 	        sum=sum+arr[i];
 	    }
+//then i stored the sum of array elements in a variable sum
+//if it was greater or equal to zero the output says YES
 	    if(sum>=0){
 	        cout<<"YES"<<endl;
 	    }
+//else no
+
 	    else{
 	        cout<<"NO"<<endl;
 	    }
