48759e05d64666bd9d86745ccb7e156c1bc68f77
Mohannedkandil <mohanadmkandil@gmail.com>
2020-10-06 01:38:38 +0200

    Added C++ solution and explanation for ElectronicShop in HackerRank

---
+++ Hackerrank_problems/ElectronicShop/solution3.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    /*
+         s (the amount of money Monica has),  
+         n (the number of keyboard brands) and  
+         m (the number of USB drive brands). 
+    */
+    int s, n, m, a, res = -1, keyboard[1001], usb[1001];
+
+    cin >> s >> n >> m;
+    for (int i = 0; i < n; i++)cin >> keyboard[i];
+    for (int i = 0; i < m; i++)cin >> usb[i];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (keyboard[i] + usb[j] <= s)res = max(res, keyboard[i] + usb[j]);
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
