68d8022c7f2d47391b3da13a0ee353243d9b331c
Divyanshu <divyanshubargali@gmail.com>
2020-10-05 19:52:16 +0530

    Added solution logic for Sherlock and valid strings

--- Hackerrank_problems/Sherlock and valid strings/solution.py
+++ Hackerrank_problems/Sherlock and valid strings/solution.py
@@ -1,6 +1,16 @@
+# Logic
+# Create a frequency list for all alphabets,
+# next compare the frequency of all alphabets from b to z with
+# frquency of a, iteratively. If the frequencies don't match
+# subtract the a's frequency from the current letter's frequency.
+# Compare the frequency of the letter with a's frequency again, if they
+# don't yet match increment diff_freqs. At the end of loop, if there's more
+# than one letter with different frequnecy than a's, diff_freqs will
+# be more than 1 and the string will be invalid!
+
+
 def isValid(s):
     freq = [0] * 26
-    # create frequency list of all letters
     for i in s:
         freq[ord(i) - ord('a')] += 1
     diff_freqs = 0
