17626c6798871ce2690912b56dcf3fe1a2674f35
Abhishek Verma <abhishekverma1610@gmail.com>
2020-10-05 15:47:21 +0530

    Update solution.py

--- LeetCode_problems/First_Unique_Character_in_a_String/solution.py
+++ LeetCode_problems/First_Unique_Character_in_a_String/solution.py
@@ -6,4 +6,7 @@ class Solution:
             if ct[s[i]]==1:
                 return i
         return -1
+# Counter makes a frequency map of letters in string
+# we iterate over the letters of the string and check if it has only 1 count, then return the first one
+# else if no count is 1, we return -1 because no letter is unique
         
