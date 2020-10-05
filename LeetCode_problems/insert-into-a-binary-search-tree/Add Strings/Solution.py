7bcd801d3033f21caae0b6e66531dcb62c56b6b4
shrey <modishrey007@gmail.com>
2020-10-05 12:21:22 +0530

    Added Leetcode Problems

---
+++ LeetCode_problems/insert-into-a-binary-search-tree/Add Strings/Solution.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 2020
+
+@author: Shrey1608
+"""
+'''Approach : 1) recursion(Time complexity= O(H)(h is height of the tree)'''
+
+
+
+#Solution : Recursion
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        if root.val> val:
+            root.left=self.insertIntoBST(root.left,val)
+        else:
+            root.right=self.insertIntoBST(root.right,val)
+        return root
