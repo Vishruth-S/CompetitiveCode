b3758ec50e503458e16139e4d8c88eb10e324be3
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-10-05 10:30:38 +0530

    Add Commenting

--- Hackerrank_problems/MinimumHeightOFTriangle/solution.py
+++ Hackerrank_problems/MinimumHeightOFTriangle/solution.py
@@ -1,7 +1,11 @@
-import math
-b, a = map(int, input().split())
-h = (2*a)/(b)
+import math  # To use ceil function
+
+b, a = map(int, input().split())  # To take input
+
+h = (2*a)/(b)  # Getting height
+
 if h > int(h):
     print(math.ceil(h))
+
 else:
     print("%.0f" % h)
