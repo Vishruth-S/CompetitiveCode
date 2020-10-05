8db36dfe1b571fdcaf21f4aec8af7360d68b6608
Mrityunjoy Chowdhury <mrityunjoy.189301071@muj.manipal.edu>
2020-10-05 23:31:05 +0530

    added problem solution Fence

---
+++ Codeforces_problems/Fence/problem.md
@@ -0,0 +1,3 @@
+#Fence - Round 675
+
+link- https://codeforces.com/contest/1422/problem/A
---
+++ Codeforces_problems/Fence/solutions.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t,a,b,c,d,i,j;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c;
+        d=a+b+c;
+        cout<<d-1<<endl;
+    }
+ 
+    return 0;
+ 
+}
