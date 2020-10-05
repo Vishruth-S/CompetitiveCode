4bb98d14274dd97edd8c3ff772f11384c6541f68
Djay1407 <43728219+Djay1407@users.noreply.github.com>
2020-10-06 00:06:32 +0530

    Update solutions.cpp

--- Codeforces_problems/Fence/solutions.cpp
+++ Codeforces_problems/Fence/solutions.cpp
@@ -1,3 +1,7 @@
+// Logic- If a,b,c are in a straight line then d can be either greater than it or equal to it or smaller than it,
+// it wont bw possible to make a quadrilateral if d is equal or larger than a+b+c but a d shouldn't be so small
+//     such that a+d<b+c, therefore we select a value just smaller than a+b+b.
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
