19d82dae618b35a987cb3780fcc95f4a112c61b3
Darshan15071998 <43002535+Darshan15071998@users.noreply.github.com>
2020-10-05 16:57:38 +0530

    Update solution.py

--- Hackerrank_problems/Picking Numbers/solution.py
+++ Hackerrank_problems/Picking Numbers/solution.py
@@ -7,29 +7,26 @@ import re
 import sys
 
 #
-# Complete the 'pickingNumbers' function below.
 #
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY a as parameter.
 #
 
 def pickingNumbers(a):
     # Write your code here
-    ans=[]
-    count=0
-    for i in a:
+    ans=[]  #Declared a List "ans" for storing the results in the program
+    count=0 #Declared count variable as 0.
+    for i in a:    # Code for counting the Length of longest subarray where the absolute difference between any two elements is 0 or 1 (Basically means one more), and then storing it into count.
         for j in a:
             if(j==i or j==i+1):
                 count=count+1
         ans.append(count)
-        count=0
-    for i in a:
-        for j in a:
+        count=0    # Initializing count as 0 again for the next loop
+    for i in a:    # Code for counting the Length of longest subarray where the absolute difference between any two elements is 0 or 1 (Basically means one less), and then storing it into count.
+        for j in a: 
             if(j==i or j==i-1):
                 count=count+1
         ans.append(count) 
-        count=0 
-    return(max(ans))                      
+        count=0     # Initializing count as 0 again for the next loop
+    return(max(ans))# Code for returning the Length of longest subarray from the list "ans" where the absolute difference between any two elements is less than or equals to 1.                
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
