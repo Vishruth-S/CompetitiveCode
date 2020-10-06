8e4729a7b8ad050143f11c4487dfcd02d322edaf
Alex Munoz <alex.munoz905@gmail.com>
2020-10-06 16:59:53 -0400

    added 13 reasons why solution from HackerEarth. Changed Big P and Party problem.md.txt to problem.md

---
+++ Hackerrank_problems/attribute-parser/solution.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cout << "test" << endl;
+    return 0;
+}
