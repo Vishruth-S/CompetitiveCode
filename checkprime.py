acb2f6d0174553ab8dab0e46ed28a905d0a30a9b
Darshan15071998 <43002535+Darshan15071998@users.noreply.github.com>
2020-10-06 11:29:57 +0530

    Update checkprime.py

--- checkprime.py
+++ checkprime.py
@@ -1,9 +1,10 @@
-a=int(input())
-t=0
-for i in range(1,a+1):
+#Program to find out that whether a given input integer No. is Prime or Not  
+a=int(input())  #Here we are taking the input No. from the user
+t=0 #Initializing "t" as 0 for further use.
+for i in range(1,a+1): #Constructed a loop for checking a No. from 1 to the position of the No. itself to find out that whether the No. of multiples are exactly 2 or not.
   if(a%i==0)
     t=t+1
-if(t==2):
+if(t==2): #Added an if-else statement for returning the answer as "Prime" or "Not Prime",depending on the value of "t". 
   print(No. is Prime)
 else:
   print(No. is Not Prime)
