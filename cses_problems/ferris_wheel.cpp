261b985597bd8295b8b7bdd1096073cfc0ad1c70
Bhavesh <49167120+SeekerHub@users.noreply.github.com>
2020-10-06 23:21:51 +0530

    Added comments

--- cses_problems/ferris_wheel.cpp
+++ cses_problems/ferris_wheel.cpp
@@ -1,5 +1,10 @@
+// n---> stores the array input or the weights of individual riding.
+// x----> stores the weight limit of gondola.
+
+// importing libraries
 #include<bits/stdc++.h>
 using namespace std;
+
 
 int main(){
     int n, x;
@@ -8,14 +13,15 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
+    #Sorting the array
     sort(a, a+n);
     long long tot = 0;
-    long long count = 0; 
+    long long count = 0; // initialize the count variable to keep track of the reequired gondolas.
     if (n==1) cout<<"1";
     else{   
         int i=0,j=n-1;
         while(i<=j){
-            tot=a[i]+a[j];
+            tot=a[i]+a[j]; // In each iteration it is important to calculate the sum of i-th front and j-th back element. 
             if(tot<=x){
                 count++;
                 i++;
