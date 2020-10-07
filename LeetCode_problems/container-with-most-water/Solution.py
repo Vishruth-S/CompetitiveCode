b4882f389e9d35b4e2b9b0344d0580516fc37dac
Shrey Modi <45357209+shrey1608@users.noreply.github.com>
2020-10-07 21:47:15 +0530

    Update Solution.py

--- LeetCode_problems/container-with-most-water/Solution.py
+++ LeetCode_problems/container-with-most-water/Solution.py
@@ -3,9 +3,9 @@
 Created on Mon Oct  5 2020
 
 @author: Shrey1608
-# Approach : 1) Brute force (Time complexity= O(n^2) || space complexity = O(1))
-#            2) Two pointer method (Time Time complexity= O(n) || space complexity = O(1))
-             
+# Approach : 1) Brute force is basically using 2 for loops to run the code  (Time complexity= O(n^2) || space complexity = O(1))
+#            2) Two pointer method is a method in which we take to elements i.e 1st and last and then  compare it to each other to get the desired value (Time Time complexity= O(n) || space complexity = O(1))
+#              
 
 # Solution:(Two pointer method)
 
