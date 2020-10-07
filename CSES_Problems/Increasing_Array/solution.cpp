f4e65c57fcdda594f124ce9de9b7d0d04c52e264
Vishruth Subramanian <vs.vishruth.s@gmail.com>
2020-10-08 00:27:31 +0530

    Formatted comments and fixed spelling error

--- CSES_Problems/Increasing_Array/solution.cpp
+++ CSES_Problems/Increasing_Array/solution.cpp
@@ -13,7 +13,8 @@ int main(){
         for(int i = 0; i<n-1; i++){ 
             count = count  + max(0, v[i]-v[i+1]); //The difference between current_element and next element will be the number of time we have to increment.
                                                   
-            if (v[i+1]<v[i]) v[i+1] = v[i]; // Here we update the next element which is the (i+1)th element, because after increment it must be equal to its previous element in account of minimum coun.
+            if (v[i+1]<v[i]) v[i+1] = v[i]; // Here we update the next element which is the (i+1)th element, 
+                                            // because after increment it must be equal to its previous element in account of minimum count.
         }
         cout<<count;
     }
