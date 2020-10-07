c4f2e97293a6a1767efab43f99229cdfdbf265b3
Bhavesh <49167120+SeekerHub@users.noreply.github.com>
2020-10-07 23:47:14 +0530

    Update solution.cpp

--- CSES_Problems/Increasing_Array/solution.cpp
+++ CSES_Problems/Increasing_Array/solution.cpp
@@ -1,8 +1,5 @@
-/*
 
-
-*/
-
+/*Importing the libraries*/
 #include<bits/stdc++.h>
 using namespace std;
  
@@ -11,11 +8,12 @@ int main(){
     cin>>n;
     vector<int> v(n);
     for(auto &i:v) cin>>i;
-    if(n!=1){
+    if(n!=1){ 
         long long count = 0;
         for(int i = 0; i<n-1; i++){ 
-            count = count  + max(0, v[i]-v[i+1]);
-            if (v[i+1]<v[i]) v[i+1] = v[i];
+            count = count  + max(0, v[i]-v[i+1]); //The difference between current_element and next element will be the number of time we have to increment.
+                                                  
+            if (v[i+1]<v[i]) v[i+1] = v[i]; // Here we update the next element which is the (i+1)th element, because after increment it must be equal to its previous element in account of minimum coun.
         }
         cout<<count;
     }
