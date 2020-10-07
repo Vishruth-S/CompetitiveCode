17c0388a658968df2e0ebcccec318b9058aff358
Ritika Jain <jainritika181999@gmail.com>
2020-10-07 21:06:42 +0530

    comments added

--- Hackerrank_problems/Cats and a Mouse/solution.py
+++ Hackerrank_problems/Cats and a Mouse/solution.py
@@ -5,9 +5,9 @@ import re
 import sys
 
 def catAndMouse(x, y, z):
-    if(abs(z-x)==abs(z-y)):
-        return "Mouse C"
-    elif(abs(z-x)>abs(z-y)):
+    if(abs(z-x)==abs(z-y)):   # checking whether both the cats reach at the same time or not
+        return "Mouse C"      # if yes, returns Mouse C
+    elif(abs(z-x)>abs(z-y)):  # checking if the cat B reaches earlier or Cat A
         return "Cat B"
     else:
         return "Cat A"
@@ -15,7 +15,7 @@ def catAndMouse(x, y, z):
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    q = int(input())
+    q = int(input())        # input the number of test cases
 
     for q_itr in range(q):
         xyz = input().split()
