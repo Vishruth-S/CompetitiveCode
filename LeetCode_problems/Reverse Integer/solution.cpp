bf14fbad4d1eb17931cc423e5a5647ecc7489ca2
lusmoura <luisamoura@usp.br>
2020-10-07 10:28:01 -0300

    Adding reverse integer explanation

--- LeetCode_problems/Reverse Integer/solution.cpp
+++ LeetCode_problems/Reverse Integer/solution.cpp
@@ -1,4 +1,40 @@
 class Solution {
+/*
+We use an auxiliary variable to store the reversed number.
+The original number is shifited to the left (by dividing by 10) so we can get the digits.
+The auxiliary number is shifited to the right (by multiplying by 10) so we can add the digits.
+
+Exemple:
+x = 1234
+aux = 0
+
+Iteration 1:
+digit = 4
+aux = aux * 10 = 0 * 10 = 0
+aux = aux + digit = 0 + 4 = 4
+x = x/10 = 1234/10 = 123
+
+Iteration 2:
+digit = 3
+aux = aux * 10 = 4 * 10 = 40
+aux = aux + digit = 40 + 3 = 43
+x = x/10 = 123/10 = 12
+
+Iteration 3:
+digit = 2
+aux = aux * 10 = 43 * 10 = 430
+aux = aux + digit = 430 + 2 = 432
+x = x/10 = 12/10 = 1
+
+Iteration 4:
+digit = 1
+aux = aux * 10 = 432 * 10 = 4320
+aux = aux + digit = 4320 + 1 = 4321
+x = x/10 = 1/10 = 0
+
+Answer: 4321
+*/
+
 public:
     int reverse(int x) {
         long long int reversed = 0;
