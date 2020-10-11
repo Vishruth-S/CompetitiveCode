d5fe72870e04dffb5e940c631a5ecbd44c093658
Vishruth Subramanian <vs.vishruth.s@gmail.com>
2020-10-11 19:26:04 +0530

    Added missing main

--- Hackerrank_problems/kangaroo/kangaroo_solution3.py
+++ Hackerrank_problems/kangaroo/kangaroo_solution3.py
@@ -3,6 +3,12 @@ case 1: When both kangaroo meets at the same location then we have to say "YES".
 case 2: If they are not going to meet in the given distance then "NO".
 """
 
+import math
+import os
+import random
+import re
+import sys
+
 """ "kangaroo" function takes 4 parameters i.e.
    1st kangaroo's start location x1 and moves at a rate of v1 meters per jump.
    2nd kangaroo's start location x2 and moves at a rate of v2 meters per jump.
@@ -18,3 +24,23 @@ def kangaroo(x1, v1, x2, v2):
 
         elif count == 10000:               # if not meeting in the given distance then returns "NO"
             return "NO"
+         
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
