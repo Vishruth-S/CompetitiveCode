1fcc31c3f1c8aef175f3e58cd9467833ce389fcd
Vishruth Subramanian <vs.vishruth.s@gmail.com>
2020-10-12 10:34:21 +0530

    Formatted comments

--- HackerEarth_problems/Odd one out/solution.cpp
+++ HackerEarth_problems/Odd one out/solution.cpp
@@ -8,10 +8,10 @@ int main(){
 
 	cin >> n;
 
-    '''
+    /*
         we capture the input numbers and add them in the variable x,
         and add the odd numbers, we compare the two sums and find the missing number.
-    '''
+    */
     for(int i=1; i<n*2; i++){
         if( i % 2 == 0){
 			cin >> inp;
