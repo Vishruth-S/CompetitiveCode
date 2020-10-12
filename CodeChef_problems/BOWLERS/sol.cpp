fa4c47f22b4ea26c4231c0fbcd9d40e0e0354c29
aruna2019 <iamaruna.nbp@gmail.com>
2020-10-12 08:28:36 +0530

    added comments

--- CodeChef_problems/BOWLERS/sol.cpp
+++ CodeChef_problems/BOWLERS/sol.cpp
@@ -1,3 +1,9 @@
+/*
+Consider a cricket game with a series of N overs (numbered 1 through N) played by K players (numbered 1 through K). Each player may be the bowler for at most L overs in total, but the same player may not be the bowler for any two consecutive overs. Assign exactly one bowler to each over in such a way that these rules are satisfied or determine that no such assignment exists.
+*/
+
+// including libraries
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -15,9 +21,9 @@ int main()
     while (t--)
     {
         ll n, k, l;
-        cin >> n >> k >> l;
+        cin >> n >> k >> l; //taking input 
         vector<int> v;
-        if (k == 1 && n > 1)
+        if (k == 1 && n > 1)  	//if value of k =1 and n>1 then return -1
         {
             cout << "-1" << endl;
             continue;
@@ -27,15 +33,16 @@ int main()
             int temp = k;
             while (temp--)
             {
-                v.push_back(temp + 1);
+                v.push_back(temp + 1);  // push temp +1 in vector v
             }
         }
         if (v.size() < n)
-            cout << "-1" << endl;
+            cout << "-1" << endl; 	//if size of the vector is less than n print -1
+
         else
         {
             for (int i = 0; i < n; i++)
-                cout << v[i] << ' ';
+                cout << v[i] << ' '; 	//else print elements in  v vector
             cout << endl;
         }
     }
