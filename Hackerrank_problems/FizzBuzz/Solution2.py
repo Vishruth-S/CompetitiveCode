a0522b62415c4d2e3f3d47d52743145ff1c7c09e
Franklin Ikeh <69267493+fr4nkl1n-1k3h@users.noreply.github.com>
2020-10-14 19:14:25 +0100

    Update Solution2.py

--- Hackerrank_problems/FizzBuzz/Solution2.py
+++ Hackerrank_problems/FizzBuzz/Solution2.py
@@ -1,18 +1,15 @@
 def fizzbuzz(x):
-	"""FizzBuzz challenge"""
-	for i in range(x):
-		i += 1
-		if i % 3 == 0 and i % 5 == 0:
-			print("fizzbuzz")
-			
-		elif i % 3 == 0:
-			print("fizz")
-				
-		elif i % 5 == 0:
-			print("buzz")
-			
-		else:
-			print(i)
+"""FizzBuzz challenge"""
+    for i in range(x):
+        i += 1
+	if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+	elif i % 3 == 0:
+            print("fizz")				
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
 			
 			
 fizzbuzz(100)
