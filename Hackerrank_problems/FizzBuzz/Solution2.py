106fc391956c09f81f7f95eae579e77d1ace1014
Franklin Ikeh <69267493+fr4nkl1n-1k3h@users.noreply.github.com>
2020-10-14 19:17:24 +0100

    Added comments

--- Hackerrank_problems/FizzBuzz/Solution2.py
+++ Hackerrank_problems/FizzBuzz/Solution2.py
@@ -1,5 +1,10 @@
 def fizzbuzz(x):
-"""FizzBuzz challenge"""
+"""FizzBuzz challenge
+scan through 1 to 100 and tell which numbers are fizz, buzz, or fizzbuzz
+fizz - a number divisible by 3
+buzz - a number divisible by 5
+fizzbuzz - a number divisible by 5 and 3
+"""
     for i in range(x):
         i += 1
 	if i % 3 == 0 and i % 5 == 0:
