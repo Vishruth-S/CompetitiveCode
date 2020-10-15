4bf2fd9b6ab455c80b0adb5a5747527d59be6952
SayakMandal <shinigamisayak2001.ju@gmail.com>
2020-10-15 12:57:45 +0530

    Added solution to a code_forces problem

---
+++ Codeforces_problems/Good_Number/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main(){
+    long n,val,count=0;
+    cin>>n>>val;
+    for(int i=0;i<n;i++){
+        long num;
+        cin>>num;
+        set<int> S;
+        while(num>0){
+            S.insert(num%10);//takes only unique digits of the number
+            num=num/10;
+        }
+        int flag=0;
+        for(int j=0;j<=val;j++){
+            if(S.find(j)==S.end()){
+                flag=1;
+            }
+        }
+        if(!flag){
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
