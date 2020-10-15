9cb20cdca0480e331e324632d0b9cad52e6c6bc9
Sohan Kundu <sohankundu99@gmail.com>
2020-10-15 11:55:12 +0530

    Update solution.py

--- Hackerrank_problems/diagonal-difference/solution.py
+++ Hackerrank_problems/diagonal-difference/solution.py
@@ -6,20 +6,20 @@ import random
 import re
 import sys
 
-#
-# Complete the 'diagonalDifference' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
+# The function accepts 2D_INTEGER_ARRAY arr as parameter and returns an integer as the diagonal difference.
 
 def diagonalDifference(arr):
-    # Write your code here
+    #Initialising the sum of left diagonal elements to be 0
     left = 0
+    #Initialising the sum of left diagonal elements to be 0
     right = 0
     for i in range(len(arr)):
+        #Iterating theough the rows of the 2D Array
         left += arr[i][i]
+        #Adding the elements of the left diagonals
         right += arr[i][len(arr)-i-1]
+        #Adding the elements of the right diagonal
+    #Returning the absolute difference between the sum of the left diagonal elements and the sum of the right diagonal elements
     return abs(left-right)
 
 
@@ -27,14 +27,18 @@ if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
+    #Accepting the number of rows of the 2D Array
 
     arr = []
 
     for _ in range(n):
         arr.append(list(map(int, input().rstrip().split())))
+        #Accepting the rows of the 2D Array
 
     result = diagonalDifference(arr)
+    #Passing the 2D array to the "diagonalDifference" function and storing the result
 
     fptr.write(str(result) + '\n')
+    #Printing the result
 
     fptr.close()
