f37b0f22c10f2b2594dccf7d1bff3d53a837f1ab
SayakMandal <shinigamisayak2001.ju@gmail.com>
2020-10-15 13:05:47 +0530

    Added solution to a Codeforces problem

--- Codeforces_problems/Good_Number/solution.cpp
+++ Codeforces_problems/Good_Number/solution.cpp
@@ -1,4 +1,6 @@
 #include<bits\stdc++.h>
+/*used a set to take take unique digits of a number in sorted order(increasing)
+And checked if the iterator returned by the last value of matches with the iterator returned by set end*/
 using namespace std;
 int main(){
     long n,val,count=0;
