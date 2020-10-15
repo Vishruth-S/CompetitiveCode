a98013df47cb895297b786139bf84df7aaca21c5
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2020-10-15 22:51:08 +0530

    Update Solution.java

--- Math and Strings Problems/Armstrong Number Problem/Solution.java
+++ Math and Strings Problems/Armstrong Number Problem/Solution.java
@@ -11,20 +11,20 @@ class armstrong
             System.out.println("\fEnter a number to check wether or not it is an Armstrong Number");
             System.out.println("ENTER non 3-digit no. TO EXIT");
             int x=scan.nextInt();
-            if(x<=999 && x>=100)
+            if(x<=999 && x>=100)        //check if the given no. is 3 digit or not
             check=true; 
             else
             check=false;
             int a,b,c;
-            a=x/100;
-            b=(x/10)-(a*10);
-            c=x-(a*100)-(b*10);
-            if((a*a*a)+(b*b*b)+(c*c*c)==x)
+            a=x/100;                    //hundred's place
+            b=(x/10)-(a*10);            //Ten's place
+            c=x-(a*100)-(b*10);         //Unit's place
+            if((a*a*a)+(b*b*b)+(c*c*c)==x)  //condition for armstrong no.
             System.out.println("Number is an armstrong number \nHit ENTER to continue");
             else
             System.out.println("Number is not an armstrong number \nHit ENTER to EXIT");
             String s=scan.nextLine();
             s=scan.nextLine();
-        }while(check==true);
+        }while(check==true);   //to exit only after all required nos. are changed
     }
 }
