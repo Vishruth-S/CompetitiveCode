2f7bd81d4c4d87d60b928309e2921eec21a036f5
Shouryan Sharma <56246301+lio2011@users.noreply.github.com>
2020-10-15 12:05:35 +0530

    Update Solution.cpp

--- Hackerrank_problems/Operators/Solution.cpp
+++ Hackerrank_problems/Operators/Solution.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Complete the solve function below.
+// Given below is th solve function
 void solve(double meal_cost, int tip_percent, int tax_percent) {
-float tip = (meal_cost*tip_percent)/100;
-float tax = (meal_cost*tax_percent)/100;
-float total = meal_cost + tax + tip;
-cout<<round(total);
+float tip = (meal_cost*tip_percent)/100; //defining tip = mealcost*tip_percent/100
+float tax = (meal_cost*tax_percent)/100;//defining tax = mealcost*tax_percent/100
+float total = meal_cost + tax + tip;//defining total as sum of meal_cost +tip +tax
+cout<<round(total);//printing round off of total since total is not an integeral value
 }
 
 int main()
 {
     double meal_cost;
     cin >> meal_cost;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');//ignores the input buffer for next input
 
     int tip_percent;
     cin >> tip_percent;
@@ -23,7 +23,7 @@ int main()
     cin >> tax_percent;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    solve(meal_cost, tip_percent, tax_percent);
+    solve(meal_cost, tip_percent, tax_percent);//function call
 
     return 0;
 }
