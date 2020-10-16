f773c05ac2a3d4cedd4588a3eca926719e9c6ab8
Sakshi Jain <62291788+im-sakshijain@users.noreply.github.com>
2020-10-16 23:52:52 +0530

    Update SOLUTION.c

--- Hackerrank_problems/Bitwise_operator/SOLUTION.c
+++ Hackerrank_problems/Bitwise_operator/SOLUTION.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //header file to use string functions
 #include <math.h>
 #include <stdlib.h>
 //Complete the following function.
 
 //function definition
-void calculate_the_maximum(int n, int k) {
+void calculate_the_maximum(int n, int k) //function prints the maximum values for the and, or and xor comparisons{
+    //int n: the highest number to consider
+    //int k: the result of a comparison must be lower than this number to be considered
     int i,j,m1=0,m2=0,m3=0; //variables declared
-    for(i=1;i<=n;i++){
-    for(j=i+1;j<=n;j++){
+    for(i=1;i<=n;i++)//loop to count from 1 to n 
+    {
+    for(j=i+1;j<=n;j++)
+    //loop to count from i+1 to n
+    {
         int and =i&j;
         int or =i|j;
         int xor =i^j;
         if(and<k && and>m1){
-            m1=and;
+            m1=and; //m1 is the largest value for and
         }
         if(or<k && or>m2){
-            m2=or;
+            m2=or; //m2 is the largest value for or
         }
         if(xor<k && xor>m3){
-            m3=xor;
+            m3=xor; //m3 is the largest value for xor
         }
     }
     }
