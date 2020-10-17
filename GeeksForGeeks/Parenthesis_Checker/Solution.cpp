3591451b4ca2ed62c3b08dc7f0c293d85b51bbc0
Mohd Shafahad <42795017+mohdshafahad@users.noreply.github.com>
2020-10-17 12:54:41 +0530

    Update Solution.cpp

--- GeeksForGeeks/Parenthesis_Checker/Solution.cpp
+++ GeeksForGeeks/Parenthesis_Checker/Solution.cpp
@@ -1,22 +1,11 @@
-#include<iostream>
-#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
-
-// This function checks if pair of bracket are matching or not
-bool areMatchingBrackets(char a,char b){
-  if(a=='('&&b==')')
-  return true;
-  if(a=='['&&b==']')
-  return true;
-  if(a=='{'&&b=='}')
-  return true;
-  return  false;
-}
 
 int main()
 {
   int t; // t is no of test cases
   cin>>t;
+ 
   while(t--)
   {
     string s;
@@ -24,7 +13,7 @@ int main()
     stack<char> st; // declare stack st
     for(int i=0;i<s.length();i++)  //   iterate through the string
     {
-      if(!st.empty() && areMatchingBrackets(st.top(),s[i])) //If the current character is a closing bracket (‘)‘ or ‘}‘ or ‘]‘) then check if it is matching bracket.
+      if(!st.empty() && st.top()==s[i]) //If the current character is a closing bracket (‘)‘ or ‘}‘ or ‘]‘) then check if it is matching bracket.
       st.pop();    // if matching bracket then pop from the stack
       else
       st.push(s[i]); // If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
@@ -34,6 +23,5 @@ int main()
     else
     cout<<"not balanced"<<endl;
   }
-
   return 0;
 }
