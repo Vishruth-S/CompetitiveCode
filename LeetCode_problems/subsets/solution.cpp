380048648eef6bfa0574235ec8ca63db190187fa
anjali0112 <60574769+anjali0112@users.noreply.github.com>
2020-10-22 07:26:26 +0530

    Update solution.cpp

--- LeetCode_problems/subsets/solution.cpp
+++ LeetCode_problems/subsets/solution.cpp
@@ -1,7 +1,7 @@
 /*time complexity-O(n*2^n) , space complexity(n*2^n)*/
-/*1-total number of subsets will be-(2^n)+1 (where n is array size)
-2-upper bound of a k bit number will be-(2^k)
-3-the binary representation of numbers from 1 to 2^k gives permutations of set bits in k bit number.
+/*1-total number of subsets will be-(2^n)(where n is array size)
+2-upper bound of a k bit number will be-(2^k)-1
+3-the binary representation of numbers from 1 to 2^k -1 gives permutations of set bits in k bit number.
 4-so we can iterate from 1 to 2^n-1 and each iteraton we can find which permutation it represents.*/
 class Solution {
 public:
