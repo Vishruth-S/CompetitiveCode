669af70682afa202dad5fd772c82f9d39463b378
Yashi Srivastava <yashi.srivastava1234@gmail.com>
2020-10-23 21:46:37 +0530

    Update solution.cpp

--- LeetCode_problems/Count and Say/solution.cpp
+++ LeetCode_problems/Count and Say/solution.cpp
@@ -1,9 +1,11 @@
 string Solution::countAndSay(int A) {
     string s[A+2];
+    // initialising strings
     s[1] = "1";
     s[2] = "11";
     for(int i = 3; i <= A; i++) {
         s[i] = "";
+        // To find identical consecutive characters
         for(int j = 0; j < s[i-1].length(); j++) {
             char x = s[i-1][j];
             int count = 1;
@@ -11,6 +13,7 @@ string Solution::countAndSay(int A) {
                 count++;
                 j++;
             }
+            // Counting letter occurences
             string sCount = "";
             while(count > 0) {
                 int y = count % 10;
@@ -18,6 +21,7 @@ string Solution::countAndSay(int A) {
                 char c = y + '0';
                 sCount.push_back(c);
             }
+            // Adding the letters
             string iCount = "";
             for(int j = sCount.length() - 1; j >= 0; j--) {
                 iCount += sCount[j];
