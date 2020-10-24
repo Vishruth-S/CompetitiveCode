bdcc56f6f8c260ab1b27b0c5a139181e8bb3d1fe
unknown <himanshu59cool@gmail.com>
2020-10-25 00:30:02 +0530

    Added Remove Element Solution with explination

--- LeetCode_problems/Remove Element/solution.cpp
+++ LeetCode_problems/Remove Element/solution.cpp
@@ -1,14 +1,19 @@
+
+//Remove Element
+//here we are given an array containing some elements along with the value to be removed from the array
+//and then return the count of remaining elements
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int c=0;
-        for (auto i = nums.begin(); i != nums.end(); ++i) { 
+        int c=0;//initialize the count to be 0
+        for (auto i = nums.begin(); i != nums.end(); ++i) { //traversing the nums vector 
         if (*i  == val) { 
-            nums.erase(i);
-            i--;
+            nums.erase(i);   //if the iterator points to the value to be removed then we delete it 
+            i--;  //decrement the iterator by 1
         } 
-        elsec++;
+        else c++;//increment the count by 1
     } 
-  return c;
+  return c; //here we return the count of remaining elements
     }
 };
