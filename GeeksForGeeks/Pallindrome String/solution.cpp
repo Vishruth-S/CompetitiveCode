22dee1bb18d0fe2afd131555746791e747e95071
FFI <ffi@iitism.ac.in>
2020-10-26 15:02:59 +0530

    Updated c++ solution

--- GeeksForGeeks/Pallindrome String/solution.cpp
+++ GeeksForGeeks/Pallindrome String/solution.cpp
@@ -41,47 +41,4 @@ int main() {
             continue;
     }
 	return 0;
-}#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int t;
-    cin >> t;
-    while(t--){
-        string s;
-        cin >> s;
-        stack<char> st;
-        int n = s.size();
-        for(int i=0;i<n;i++){
-            if(st.empty()) st.push(s[i]);
-            else if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
-            else if(s[i]==')'){
-                if(st.top()=='(') st.pop();
-                else {
-                    cout << "not balanced" << endl;
-                    goto last;
-                }
-            }
-            else if(s[i]==']'){
-                if(st.top()=='[') st.pop();
-                else {
-                    cout << "not balanced" << endl;
-                    goto last;
-                }
-            }
-            else if(s[i]=='}'){
-                if(st.top()=='{') st.pop();
-                else {
-                    cout << "not balanced" << endl;
-                    goto last;
-                }
-            }
-        }
-        // cout << st.size() << endl;
-        if(st.size()==0) cout << "balanced" << endl;
-        else cout << "not balanced" << endl;
-        last:
-            continue;
-    }
-	return 0;
 }
