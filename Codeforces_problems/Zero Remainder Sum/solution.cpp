a876d766d61467a766ffdd638299c28a0549e544
Gabriel Schneider <gabrielschneider100@protonmail.com>
2020-10-27 07:44:31 -0300

    Update solution.cpp

--- Codeforces_problems/Zero Remainder Sum/solution.cpp
+++ Codeforces_problems/Zero Remainder Sum/solution.cpp
@@ -16,24 +16,38 @@ int T;
 
 int dp[N][N][N][N];
 
-// this is a classic dp problem, very similar to the knapsack problem
+// this is a classic dp problem, very similar to the knapsack problem.
+// the DP's state will be the current column, row, how many numbers we picked
+// in the current row and the mod of the current sum.
 int solve(int a, int b, int left, int mod) {
+    // if we have already visited the current state we just return it's value.
     if(dp[a][b][left][mod]!=-1) return dp[a][b][left][mod];
-
+    
+    // we have three transitions:
+    // we can pick an element, not pick an element,
+    // and transition from one row to the next
     int ans;
     if(a == n) { 
+        // the answer will be mod 0.
         if(mod == 0) return 0;
+        // we don't want the other mods.
         else return -inf;
     }
+    // transision from one row to the next
     else if(b == m) ans = solve(a+1,0,T,mod);
+    // don't pick the current element and move to the next column
     else if(left == 0) ans = solve(a,b+1,left,mod);
     else {
+        // pick the current element
         int x = mat[a][b] + solve(a,b+1,left-1,(mod+mat[a][b])%k);
+        // don't pick the current element and move to the next column
         int y = solve(a,b+1,left,mod);
-
+        
+        // store and return the maximum, because we want to maximize.
         return dp[a][b][left][mod] = max(x,y);
     }
 
+    // storing the current state's value.
     return dp[a][b][left][mod] = ans;
 }
 
@@ -47,7 +61,7 @@ int main() {
             scanf("%d",&mat[i][j]);
         }
     }
-
+    // -1 in the array means that we haven't visited that state
     memset(dp, -1, sizeof(dp));
 
     int ans = solve(0,0,T,0);
