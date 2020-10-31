b53d5053306acbd9bc24d6afd1aab4830ca36520
Aldrin Jenson <aldrinjenson@gmail.com>
2020-10-31 23:23:32 +0530

    Remove unnecessary comment

--- Codewars_problems/Where my Anagrams At/solution.js
+++ Codewars_problems/Where my Anagrams At/solution.js
@@ -1,6 +1,6 @@
 function anagrams(word, words) {
   let anagrams = [];
-  const sortedWord = word.split("").sort().join(""); // sorted word
+  const sortedWord = word.split("").sort().join(""); 
   for (const w of words) {
     if (
       w.length === word.length && // if the length is not same, then there is no need to check for sorted condition
