144f1234b553f40df040ef27144e0522bb837260
Ekamjit SIngh <s.ekam97@gmail.com>
2020-10-31 12:36:33 +0530

    added comments in solution code

--- InterviewBit_problems/TwoPointers/remove_duplicate_elements_II/solution.cpp
+++ InterviewBit_problems/TwoPointers/remove_duplicate_elements_II/solution.cpp
@@ -8,18 +8,22 @@ int Solution::removeDuplicates(vector<int> &A) {
     if(A.size() == 0 || A.size() == 1){
         return n;
     }
+    // Initialise the pointers
     int i =0, j =0;
     while( j<A.size()){
+        // Increment 2nd pointer untill the number repeats
         if(A[i] == A[j]){
             j++ ;
             continue;
         }
+        // If number repeats more than twice, erase the extra repeatations
         if(j > i+2){
             A.erase(A.begin()+i+2, A.begin()+j);
             j = i;
         }
         i = j;
     }
+    // Erase extra repeatations, if number at the end of array also repeats more than twice.
     if( (j-i)> 2){
         A.erase( A.begin() + i+2, A.end());
     }
