98640ca1f0d945514e929e8a0c1f4d90ef372c8a
Aldrin Jenson <aldrinjenson@gmail.com>
2020-10-31 23:19:56 +0530

    Add comments
    - Minor refactoring

--- Codewars_problems/Where my Anagrams At/solution.js
+++ Codewars_problems/Where my Anagrams At/solution.js
@@ -1,10 +1,13 @@
 function anagrams(word, words) {
-  let ans = [];
-  let sortedWord = word.split("").sort().join("");
+  let anagrams = [];
+  const sortedWord = word.split("").sort().join(""); // sorted word
   for (const w of words) {
-    if (w.length === word.length && w.split("").sort().join("") === sortedWord) {
-      ans.push(w);
+    if (
+      w.length === word.length && // if the length is not same, then there is no need to check for sorted condition
+      w.split("").sort().join("") === sortedWord
+    ) {
+      anagrams.push(w);
     }
   }
-  return ans;
+  return anagrams;
 }
